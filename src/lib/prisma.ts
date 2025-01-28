b59a3df69da03a669497c4ac90e9afa7fd5a5d8f
José Carlos Batista <josecarlos@musetra.com>
2025-01-28 07:05:40 -0300

    fix: Ajusta configurações do Prisma para conexão com Supabase

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,6 +4,14 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientType | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClientType()
+export const prisma = globalForPrisma.prisma ?? new PrismaClientType({
+  log: ['query', 'error', 'warn'],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL
+    }
+  },
+  rejectOnNotFound: false
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
