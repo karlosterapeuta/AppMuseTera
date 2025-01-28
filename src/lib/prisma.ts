c3cf827aef7b189d9603351c3e5740f311f925d1
José Carlos Batista <josecarlos@musetra.com>
2025-01-28 07:25:43 -0300

    fix: Simplifica configuração do Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,21 +4,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientType | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClientType({
-  log: [
-    { emit: 'event', level: 'query' },
-    { emit: 'stdout', level: 'error' },
-    { emit: 'stdout', level: 'info' },
-    { emit: 'stdout', level: 'warn' },
-  ],
-})
+export const prisma = globalForPrisma.prisma ?? new PrismaClientType()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-
-// Log all queries in development
-if (process.env.NODE_ENV !== 'production') {
-  prisma.$on('query', (e) => {
-    console.log('Query: ' + e.query)
-    console.log('Duration: ' + e.duration + 'ms')
-  })
-}
