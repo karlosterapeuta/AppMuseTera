920aefb112d5068dea582f099431b589361f3a06
José Carlos Batista <josecarlos@musetra.com>
2025-01-28 07:10:26 -0300

    fix: Remove opção rejectOnNotFound do Prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,13 +5,12 @@ const globalForPrisma = globalThis as unknown as {
 }
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClientType({
-  log: ['query', 'error', 'warn'],
+  log: ['error', 'warn'],
   datasources: {
     db: {
       url: process.env.DATABASE_URL
     }
-  },
-  rejectOnNotFound: false
+  }
 })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
