575d4083b5a5fbe04f447b3d0c8f2af54f3a1341
José Carlos Batista <josecarlos@musetra.com>
2025-01-28 06:56:21 -0300

    fix: Configura pgbouncer corretamente para o Supabase

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,12 +4,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientType | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClientType({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL?.replace(':6543', ':5432') || ''
-    }
-  }
-})
+export const prisma = globalForPrisma.prisma ?? new PrismaClientType()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
