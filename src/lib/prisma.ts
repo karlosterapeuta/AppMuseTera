57d39db826addb0ee197dc3087c7511a66b58276
José Carlos Batista <josecarlos@musetra.com>
2025-01-28 08:43:33 -0300

    fix: Força uso da URL direta no PrismaClient

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,7 +6,11 @@ const globalForPrisma = globalThis as unknown as {
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClientType({
   log: ['error', 'warn'],
-  datasourceUrl: process.env.DATABASE_URL
+  datasources: {
+    db: {
+      url: process.env.DIRECT_URL
+    }
+  }
 })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
