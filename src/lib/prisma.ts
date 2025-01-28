3aaf05e6ce3e08060211adacb7a835835499be97
José Carlos Batista <josecarlos@musetra.com>
2025-01-28 06:48:59 -0300

    fix: Força Prisma a usar porta 5432 para conexão com o banco

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,6 +4,12 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientType | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClientType()
+export const prisma = globalForPrisma.prisma ?? new PrismaClientType({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL?.replace(':6543', ':5432') || ''
+    }
+  }
+})
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
